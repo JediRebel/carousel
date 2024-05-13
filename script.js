29c9209212d89f61d5b73fe20b128119f5acd3cb
Matt Zhao <matt.zhao2010@gmail.com>
2024-05-13 20:01:10 -0300

    raw data

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Slider data
+const sliderData = [
+    { url: './images/back_1.png', title: 'In this hour, I do not believe that any darkness will endure.', color: 'rgb(100, 67, 68)' },
+    { url: './images/back_2.png', title: 'Even the smallest person can change the course of the future.', color: 'rgb(43, 35, 26)' },
+    { url: './images/back_3.png', title: 'I tried to save the Shire, and it has been saved.', color: 'rgb(36, 31, 33)' },
+    { url: './images/back_4.png', title: "All's well that ends better.", color: 'rgb(139, 98, 66)' },
+    { url: './images/back_5.png', title: 'Let him not vow to walk in the dark, who has not seen the nightfall.', color: 'rgb(67, 90, 92)' },
+    { url: './images/back_6.png', title: 'Deeds will not be less valiant because they are unpraised.', color: 'rgb(166, 131, 143)' },
+    { url: './images/back_7.png', title: 'Not all those who wander are lost.', color: 'rgb(53, 29, 25)' },
+    { url: './images/back_9.png', title: 'May it be a light to you in dark places, when all other lights go out.', color: 'rgb(53, 29, 25)' },
+    { url: './images/back_8.png', title: 'Despair is only for those who see the end beyond all doubt. We do not.', color: 'rgb(67, 90, 92)' },
+
+]
 
